dbef5af9db6c92334cbb7ddd4ff0309fe6ca2325
Jean-Michel GURGUI <jemi33@gmail.com>
2024-03-28 08:44:15 +0100

    Update messagebas.php

--- structure/php/messagebas.php
+++ structure/php/messagebas.php
@@ -6,8 +6,8 @@ $affiche_message_bas = true;
 
  <aside class="messagebas">
 
-    <section>
-      Coucou, le code source d’iActu, version 15, est désormais disponible et consultable sur Github. Vous pouvez donc rapporter les bugs, améliorer le code, etc : <a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" id="MessageHautFaireundon" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊">&nbsp;🔗Lien code source&nbsp;</a>
+ <section>
+      Coucou, le code source d’iActu, version 15, est désormais disponible et consultable sur Github. Vous pouvez donc rapporter les bugs, améliorer le code, etc : <a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" id="MessageHautGithub" aria-label="Lien code source 😊" alt="Lien code source 😊" title="Lien code source 😊">&nbsp;🔗Lien code source&nbsp;</a>
     </section>
 
  </aside>
