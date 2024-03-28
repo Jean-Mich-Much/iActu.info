06fc7cd42c936641af2a9fc5ac60677c151cf739
Jean-Michel GURGUI <jemi33@gmail.com>
2024-03-28 10:41:57 +0100

    Update messagehaut.php

--- structure/php/messagehaut.php
+++ structure/php/messagehaut.php
@@ -1,5 +1,5 @@
 <?php
-$affiche_message_haut = true;
+$affiche_message_haut = false;
 ?>
 
 <?php if ($affiche_message_haut) { ?>
@@ -7,7 +7,7 @@ $affiche_message_haut = true;
   <section class="messagehaut">
 
     <section>
-      Coucou, le code source d’iActu, version 15, est désormais disponible et consultable sur Github. Vous pouvez donc rapporter les bugs, améliorer le code, etc : <a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" id="MessageHautGithub" aria-label="Lien code source 😊" alt="Lien code source 😊" title="Lien code source 😊">&nbsp;🔗Lien code source&nbsp;</a>
+    📣 Coucou, le code source d’iActu, version 15, est désormais disponible et consultable sur Github. Vous pouvez donc rapporter les bugs, améliorer le code, etc : <a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" id="MessageHautGithub" aria-label="Lien code source 😊" alt="Lien code source 😊" title="Lien code source 😊">&nbsp;🔗 Lien code source&nbsp;</a>💕
     </section>
 
   </section>
