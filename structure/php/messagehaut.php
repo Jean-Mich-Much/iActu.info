f6ebf99e71c6dcf4e5f90316105cae2596c33345
Jean-Michel GURGUI <jemi33@gmail.com>
2024-03-28 08:46:00 +0100

    Update messagehaut.php

--- structure/php/messagehaut.php
+++ structure/php/messagehaut.php
@@ -7,21 +7,7 @@ $affiche_message_haut = false;
   <aside class="messagehaut">
 
     <section>
-      📋 Dons mars : manque 110€
-    </section>
-
-    <section>
-      <a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" id="MessageHautFaireundon" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊">&nbsp;🎁Faire un don (manque 110€)&nbsp;</a>
-    </section>
-
-    <section>
-      <form>
-        <select aria-label="Liste des donateurs" id="MessageHautListeDonateurs" name="Liste des donateurs" size="1" title="Liste des donateurs">
-          <option title="Liste des donateurs" aria-label="Liste des donateurs" selected>👤 Philippe D. 10€ - 25/03&nbsp;</option>
-          <option title="Donateur" aria-label="Donateur">👤 Xavier M. 5€ - 05/03&nbsp;</option>
-          <option title="Donateur" aria-label="Donateur">👤 Sebastien D. 5€ - 03/03&nbsp;</option>
-        </select>
-      </form>
+      Coucou, le code source d’iActu, version 15, est désormais disponible et consultable sur Github. Vous pouvez donc rapporter les bugs, améliorer le code, etc : <a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" id="MessageHautGithub" aria-label="Lien code source 😊" alt="Lien code source 😊" title="Lien code source 😊">&nbsp;🔗Lien code source&nbsp;</a>
     </section>
 
   </aside>
