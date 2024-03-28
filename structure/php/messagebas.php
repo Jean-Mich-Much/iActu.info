d5710358bc38f0f897d28ce7c173adb37fe9e5e5
Jean-Michel GURGUI <jemi33@gmail.com>
2024-03-28 09:59:39 +0100

    Update messagebas.php

--- structure/php/messagebas.php
+++ structure/php/messagebas.php
@@ -4,12 +4,12 @@ $affiche_message_bas = true;
 
 <?php if ($affiche_message_bas) { ?>
 
- <aside class="messagebas">
+ <section class="messagebas">
 
  <section>
       Coucou, le code source d’iActu, version 15, est désormais disponible et consultable sur Github. Vous pouvez donc rapporter les bugs, améliorer le code, etc : <a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" id="MessageBasGithub" aria-label="Lien code source 😊" alt="Lien code source 😊" title="Lien code source 😊">&nbsp;🔗Lien code source&nbsp;</a>
     </section>
 
- </aside>
+ </section>
 
 <?php } ?>
