0b041751bf0537daec8c5298bc141bd327be2fb2
Jean-Michel GURGUI <jemi33@gmail.com>
2024-04-22 18:54:55 +0200

    Add files via upload

---
+++ structure/php/tv_demain_sousmenuhaut.php
@@ -0,0 +1,51 @@
+<?php
+$affiche_paypal_haut = false;
+?>
+
+<?php @include 'structure/php/messagehaut.php'; ?>
+
+<nav id="sousmenu">
+
+  <span>
+      <a href="tv.php" title="🌆Ce soir à la TV 😊">&nbsp;🌆Ce soir&nbsp;</a>
+  </span>
+
+  <span>
+      <a href="tv_programme.php" title="🌇Programme TV 😊">&nbsp;🌇Programme&nbsp;</a>
+  </span>
+
+  <span id="SousMenuHautDates">
+    <form>
+      <select aria-label="Liste des dates" id="SousMenuHautDatesSelect" name="Dates" size="1" title="Liste des dates" onChange="window.location.href=this.value">
+        <option value="#haut" title="Liste des dates" aria-label="Liste des dates" selected="">📅&nbsp;Demain soir&nbsp;
+        </option>
+        <option value="tv.php" title="Aujourd’hui" aria-label="Aujourd’hui">📋&nbsp;Aujourd’hui&nbsp;</option>
+        <option value="tv<?php echo date('j', strtotime('-1 days')) . '.html'; ?>" title="Hier" aria-label="Hier">📋&nbsp;Hier soir&nbsp;</option>
+      </select>
+    </form>
+  </span>
+
+
+  <span id="SousMenuHautDivers_ou_Paypal">
+    <?php if ($affiche_paypal_haut) { ?>
+      &nbsp;
+      <a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" id="SousMenuHautDonsPaypalHref" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊">&nbsp;🎁Faire un don (manque 85€)&nbsp;</a>
+      &nbsp;
+    <?php } else { ?>
+
+      <form>
+        <select aria-label="Divers" id="SousMenuHautDiversSelect" name="SousMenuDivers" size="1" title="Divers" onChange="window.location.href=this.value">
+          <option value="#haut" title="Divers" aria-label="Divers" selected="">📜 Liens divers&nbsp;</option>
+          <option value="https://github.com/Jean-Mich-Much/iActu.info" title="Lien GitHub" aria-label="GitHub">😸Code source&nbsp;</option>
+          <option value="#coucou" title="Lien vers le bas de la page" aria-label="Lien vers le bas de la page">⬇️ Bas de page&nbsp;</option>
+        </select>
+      </form>
+
+    <?php } ?>
+  </span>
+
+  <span id="SousMenuHautHeureMajPage">⏰&nbsp;Màj
+    <?php echo date('H:i'); ?>
+  </span>
+
+</nav>
---
+++ structure/php/tv_sousmenubas.php
@@ -0,0 +1,49 @@
+<?php @include 'structure/php/messagebas.php'; ?>
+
+<nav id="sousmenubas">
+
+  <span id="SousMenuBasListeDonateurs">
+    <form>
+      <select aria-label="Liste des donateurs" id="SousMenuBasListeDonateursSelect" name="Liste des donateurs" size="1" title="Liste des donateurs">
+        <option value="#SousMenuBasListeDonateursSelect" title="Donateur" aria-label="Donateur" selected="">👤 Jérémy B. 10€ - 05/04&nbsp;</option>
+        <option value="#SousMenuBasListeDonateursSelect" title="Donateur" aria-label="Donateur">👤 Xavier M. 5€ - 05/04&nbsp;</option>
+        <option value="#SousMenuBasListeDonateursSelect" title="Donateur" aria-label="Donateur">👤 Sebastien D. 5€ - 03/04&nbsp;</option>
+        <option value="#SousMenuBasListeDonateursSelect" title="Liste des donateurs" aria-label="Liste des donateurs">👤 David J. 85€ - 29/03&nbsp;</option>
+        <option value="#SousMenuBasListeDonateursSelect" title="Liste des donateurs" aria-label="Liste des donateurs">👤 Philippe D. M. 20€ - 29/03&nbsp;</option>
+        <option value="#SousMenuBasListeDonateursSelect" title="Liste des donateurs" aria-label="Liste des donateurs">👤 Christophe J. 5€ - 29/03&nbsp;</option>
+        <option value="#SousMenuBasListeDonateursSelect" title="Liste des donateurs" aria-label="Liste des donateurs">👤 Philippe D. 10€ - 25/03&nbsp;</option>
+        <option value="#SousMenuBasListeDonateursSelect" title="Donateur" aria-label="Donateur">👤 Xavier M. 5€ - 05/03&nbsp;</option>
+        <option value="#SousMenuBasListeDonateursSelect" title="Donateur" aria-label="Donateur">👤 Sebastien D. 5€ - 03/03&nbsp;</option>
+      </select>
+    </form>
+  </span>
+
+  <span id="SousMenuBasPaypal">
+    <a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" id="SousMenuBasPaypalLien" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊">&nbsp;🎁Faire un don&nbsp;</a>
+  </span>
+
+  <span id="SousMenuBasStatsDivers">
+    <form>
+      <select aria-label="Stats et diver" id="SousMenuBasStatsDiversSelect" name="StatsDivers" size="1" title="Stats et divers" onChange="window.location.href=this.value">
+        <option value="#SousMenuBasStatsDiversSelect" title="Stats" aria-label="Stats" selected="">📜 Stats et divers&nbsp;</option>
+        <option value="#SousMenuBasStatsDiversSelect" title="Visiteurs uniques, chaque visiteur est compté une seule fois par jour" aria-label="Visiteurs uniques">📈 Visiteurs :
+          <?php @readfile("Stats/visiteurs_jour_" . date('d') . ".txt"); ?>&nbsp;
+        </option>
+        <option value="#SousMenuBasStatsDiversSelect" title="Visiteurs uniques pour hier" aria-label="Visiteurs uniques pour hier">📈 Hier :
+          <?php @readfile("Stats/visiteurs_jour_" . date('d', strtotime("-1 day")) . ".txt"); ?>&nbsp;
+        </option>
+        <option value="#SousMenuBasStatsDiversSelect" title="Visites" aria-label="Visites">📈 Visites :
+          <?php @readfile("Stats/visites_jour_" . date('d') . ".txt"); ?>&nbsp;
+        </option>
+        <option value="#SousMenuBasStatsDiversSelect" title="Visites pour hier" aria-label="Visites pour hier">📈 Hier :
+          <?php @readfile("Stats/visites_jour_" . date('d', strtotime("-1 day")) . ".txt"); ?>&nbsp;
+        </option>
+        <option value="https://github.com/Jean-Mich-Much/iActu.info" title="Version" aria-label="Version 15">📜 Version 15&nbsp;</option>
+        <option value="https://github.com/Jean-Mich-Much/iActu.info" title="Lien GitHub" aria-label="GitHub">😸Lien code source&nbsp;</option>
+        <option value="https://github.com/Jean-Mich-Much/iActu.info" title="Copyright" aria-label="© iactu.info 2003">📰 ©️ iactu.info 2003&nbsp;</option>
+        <option value="#haut" title="Lien vers le haut de la page" aria-label="Lien vers le haut de la page">⬆️ Haut de page&nbsp;</option>
+      </select>
+    </form>
+  </span>
+
+</nav>
