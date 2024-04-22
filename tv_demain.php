78cab92df0f6cc50380dc306764df6a9c5bae04c
Jean-Michel GURGUI <jemi33@gmail.com>
2024-04-22 18:51:39 +0200

    Add files via upload

---
+++ tv_demain.php
@@ -0,0 +1,70 @@
+<?php
+$titre_page = 'TV - Demain';
+$nom_page = 'tv_demain';
+$page_active = $nom_page;
+$cache_secondes = 10800;
+?>
+
+<?php if (!file_exists($nom_page . '.html') or filemtime($nom_page . '.html') < (time() - $cache_secondes) or !file_exists($nom_page . date("j") . '.html')) { ?>
+
+    <?php ob_start(); ?>
+
+    <!DOCTYPE html>
+    <html lang="fr">
+
+        <head>
+            <?php @include 'structure/php/tv_demain_head.php'; ?>
+        </head>
+
+        <body id="body" lang="fr">
+
+            <nav id="haut">
+                <?php @include 'structure/php/navhaut.php'; ?>
+            </nav>
+            <?php @include 'structure/php/tv_demain_sousmenuhaut.php'; ?>
+            <?php @include 'structure/php/functions/unique.php'; ?>
+            <?php @include 'structure/php/functions/lit_tv_demain.php'; ?>
+
+            <main id="main_sites">
+                <article class="sites" id="articles_sites">
+                    <!-- DEBUT -->
+                    <div class="sitesPar2">
+                        <span class="siteGauche">
+                            <div class="boite">
+                                <?php lit_tv('xmltv_tnt', 'tv-01', '20:29:59', '23:59:59', '35', '2', '188', '200', false, true, true, true); ?>
+                            </div>
+                        </span>
+                    </div>
+                    <!-- FIN -->
+                </article>
+            </main>
+
+            <?php @include 'structure/php/tv_sousmenubas.php'; ?>
+            <nav id="bas">
+                <?php @include 'structure/php/navbas.php'; ?>
+            </nav>
+
+        </body>
+
+    </html>
+
+    <?php
+    $p = ob_get_clean();
+    if (
+        substr_count($p, '<!DOCTYPE html>') === 1 &&
+        substr_count($p, '</html>') === 1 &&
+        substr_count($p, 'http') >= 10 &&
+        strlen($p) >= 16384
+    ) {
+        echo $p;
+        file_put_contents($nom_page . '.html', $p);
+        @chmod($nom_page . '.html', 0775);
+        @copy($nom_page . '.html', $nom_page . date("j") . '.html');
+        @chmod($nom_page . date("j") . '.html', 0775);
+    } else {
+        readfile($nom_page . '.html');
+    }
+} else {
+    readfile($nom_page . '.html');
+} ?>
+<?php @include 'structure/php/functions/stats.php';
