cd2ead5f8f6255a46731d804f64a0b1f6de648df
Jean-Michel GURGUI <jemi33@gmail.com>
2024-05-07 18:18:18 +0200

    Add files via upload

--- structure/php/messagehaut.php
+++ structure/php/messagehaut.php
@@ -6,8 +6,8 @@ $affiche_message_haut = false;
 
   <section class="messagehaut">
 
-    <section>
-    📣 Coucou, le code source d’iActu, version 15, est désormais disponible et consultable sur Github. Vous pouvez donc rapporter les bugs, améliorer le code, etc : <a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" id="MessageHautGithub" aria-label="Lien code source 😊" alt="Lien code source 😊" title="Lien code source 😊">&nbsp;🔗 Lien code source&nbsp;</a>💕
+    <section style="align-self: center;justify-self: center;text-align: center;vertical-align: middle;width: 100%;">
+    📣 Coucou, il manque toujours 65 euros pour boucler le budget 🤒 <a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" id="SousMenuHautDonsPaypalHref" aria-label="Faire un don en étant assis, merci par avance 😊" alt="Faire un don en étant assis, merci par avance 😊" title="Faire un don en étant assis, merci par avance 😊">&nbsp;Lien Paypal &nbsp;</a>  💕
     </section>
 
   </section>
