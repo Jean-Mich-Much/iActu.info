d44a0756a9eb1d554ae016233cb0072254352962
Jean-Michel GURGUI <jemi33@gmail.com>
2025-02-24 19:30:49 +0100

    Add files via upload

--- Structure/php/modules/messages.php
+++ Structure/php/modules/messages.php
@@ -1,22 +1,27 @@
-<span class="pad16px retour_ligne_on hauteur_auto largeur_auto marges_auto">
 
-<span class="aligne"><span class="aligne gris_clairx3 opacity75pc">📎</span>&nbsp;&nbsp;Objectif février :&nbsp;<span style="display: inline;color: var(--c5);">20€</span>&nbsp;reçus sur&nbsp;<span style="display: inline;color: var(--c5);">130€</span>&nbsp;(manque&nbsp;<span style="display: inline;color: var(--c3);">110€</span>)&nbsp;&nbsp;</span>
+<span class="pad8px retour_ligne_on hauteur_auto largeur_auto marges_auto">
 
-<span class="aligne separe m4px">&#124;&nbsp;&nbsp;</span>
+<span class="aligne"><span class="aligne gris_clairx3 opacity75pc">📎</span>&nbsp;Dons :&nbsp;<span style="color: var(--c5);">25€</span>&nbsp;reçus sur&nbsp;<span style="color: var(--c5);">130€</span>&nbsp;(manque&nbsp;<span style="color: var(--c3);">105€</span>)</span>
 
- <span class="aligne"><span class="aligne gris_clairx3 opacity75pc">👤</span>&nbsp;&nbsp;Derniers donateurs : Xavier M. 5€ - 05/02, Stéphane L. 10€ - 04/02&nbsp;&nbsp;</span>
+<span class="separe m8px">&#124;</span>
 
- <span class="aligne separe m4px">&#124;</span>
+ <span class="aligne"><span class="aligne gris_clairx3 opacity75pc">👤</span>&nbsp;Donateur : Nicolas B. 5€ le 24/02</span>
 
- <span class="aligne separe m4px"><a class="aligne faireundon" href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊">&nbsp;&nbsp;<span class="aligne gris_clairx2 opacity85pc">💳&nbsp;</span>Faire un don&nbsp;&nbsp;</a></span>
+ <span class="separe m8px">&#124;</span>
 
- <span class="aligne separe m4px">&#124;</span>
+ <span class="aligne separe"><a class="aligne faireundon" href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊"><span class="aligne gris_clairx2 opacity85pc">💳</span>&nbsp;Faire un don</a></span>
 
- <a class="aligne" href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" alt="Github 😊" title="Github 😊">&nbsp;&nbsp;<span class="aligne gris_clair opacity75pc">😸&nbsp;</span>
-  Github&nbsp;&nbsp;</a>
+ <?php if (!$affiche_message_top) {; ?>
 
- <span class="aligne separe m4px">&#124;&nbsp;&nbsp;</span>
+ <span class="separe m8px">&#124;</span>
+ 
+ <a class="aligne" href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" alt="Github 😊" title="Github 😊"><span class="aligne gris_clair opacity75pc">😸</span>&nbsp;
+  Github</a>
+
+ <span class="separe m8px">&#124;</span>
 
  <span id="coucou" class="aligne gris_clair coucou" data-user="33imej" data-website="moc.liamg" title="Coucou 😊" aria-label="Coucou 😊" role="term"></span>
+ 
+ <?php }; ?>
 
 </span>
---
+++ Structure/php/modules/messages_top.php
@@ -0,0 +1,7 @@
+<?php $affiche_message_top = true;?>
+
+<?php if ($affiche_message_top) {; ?>
+
+<div class="messages retour_ligne_on hauteur_auto top"><?php @include 'Structure/php/modules/messages.php'; ?></div>
+
+<?php };
