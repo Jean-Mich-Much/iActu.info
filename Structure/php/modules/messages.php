c113abd234bf8416ef07ccf25874a4935b96e657
Jean-Michel GURGUI <jemi33@gmail.com>
2025-02-26 10:34:21 +0100

    Add files via upload

--- Structure/php/modules/messages.php
+++ Structure/php/modules/messages.php
@@ -1,24 +1,24 @@
 
 <span class="pad8px retour_ligne_on hauteur_auto largeur_auto marges_auto">
 
-<span class="aligne"><span class="aligne gris_clairx3 opacity75pc">📎</span>&nbsp;Dons :&nbsp;<span style="color: var(--c5);">25€</span>&nbsp;reçus sur&nbsp;<span style="color: var(--c5);">130€</span>&nbsp;(manque&nbsp;<span style="color: var(--c3);">105€</span>)</span>
+<span class="aligne">📎&nbsp;Dons :&nbsp;35€&nbsp;reçus sur&nbsp;130€&nbsp;(manque&nbsp;95€)</span>
 
-<span class="separe m8px">&#124;</span>
+<span class="aligne separe m8px">&#124;</span>
 
- <span class="aligne"><span class="aligne gris_clairx3 opacity75pc">👤</span>&nbsp;Donateur : Nicolas B. 5€ le 24/02</span>
+ <span class="aligne">👤&nbsp;Donateur : Philippe D. 10€ le 25/02</span>
 
- <span class="separe m8px">&#124;</span>
+ <span class="aligne separe m8px">&#124;</span>
 
- <span class="aligne separe"><a class="aligne faireundon" href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊"><span class="aligne gris_clairx2 opacity85pc">💳</span>&nbsp;Faire un don</a></span>
+ <span class="aligne"><a class="aligne" href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊">💳&nbsp;Faire un don</a></span>
 
  <?php if (!$affiche_message_top) {; ?>
 
- <span class="separe m8px">&#124;</span>
+ <span class="aligne separe m8px">&#124;</span>
  
- <a class="aligne" href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" alt="Github 😊" title="Github 😊"><span class="aligne gris_clair opacity75pc">😸</span>&nbsp;
+ <a class="aligne" href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" alt="Github 😊" title="Github 😊">😸&nbsp;
   Github</a>
 
- <span class="separe m8px">&#124;</span>
+ <span class="aligne separe m8px">&#124;</span>
 
  <span id="coucou" class="aligne gris_clair coucou" data-user="33imej" data-website="moc.liamg" title="Coucou 😊" aria-label="Coucou 😊" role="term"></span>
  
