98270ffa31d495a2772873e95ac027f1de485a8f
Jean-Michel GURGUI <jemi33@gmail.com>
2025-02-28 17:43:29 +0100

    Add files via upload

--- Structure/php/modules/messages.php
+++ Structure/php/modules/messages.php
@@ -1,27 +1,13 @@
 
 <span class="pad8px retour_ligne_on hauteur_auto largeur_auto marges_auto">
-
-<span class="aligne">📎&nbsp;Dernier jour&nbsp;:&nbsp;manque&nbsp;65€</span>
-
-<span class="aligne separe m8px">&nbsp;</span>
-
- <span class="aligne">👤&nbsp;Donateur : 	Johann V. 20€ le 27/02</span>
-
- <span class="aligne separe m8px">&nbsp;</span>
-
- <span class="aligne"><a class="aligne" href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊">💳&nbsp;Faire un don</a></span>
-
- <?php if (!$affiche_message_top) {; ?>
-
- <span class="aligne separe m8px">&nbsp;</span>
  
- <a class="aligne" href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" alt="Github 😊" title="Github 😊">😸&nbsp;
-  Github</a>
+<span> 📎&nbsp;Dernier jour&nbsp;:&nbsp;manque&nbsp;60€ </span>
 
- <span class="aligne separe m8px">&nbsp;</span>
+<span> 👤&nbsp;Donateur : Philippe B. 5€ le 28/02 </span>
 
- <span id="coucou" class="aligne gris_clair coucou" data-user="33imej" data-website="moc.liamg" title="Coucou 😊" aria-label="Coucou 😊" role="term"></span>
- 
- <?php }; ?>
+<span><a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊"> 💳&nbsp;Faire un don </a></span>
 
+<?php if (!$affiche_message_top) {; ?><a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" alt="Github 😊" title="Github 😊"> 😸&nbsp;Github </a><span id="coucou" class="gris_clair coucou" data-user="33imej" data-website="moc.liamg" title="Coucou 😊" aria-label="Coucou 😊" role="term"></span>
+
+<?php }; ?>
 </span>
