15cf925fd6439f341d352763ffbb26563ed8a037
Jean-Michel GURGUI <jemi33@gmail.com>
2025-03-04 16:55:43 +0100

    Add files via upload

--- theme_02_tv_prog.php
+++ theme_02_tv_prog.php
@@ -15,45 +15,36 @@ if(!file_exists($nom_page.'.html')||filemtime($nom_page.'.html')<(time()-$cache)
 <html lang="fr">
 
  <head><?php @include "Structure/php/modules/header.php"; ?><style type="text/css">
-  <?php @readfile("Structure/css/fonts.css");
-  @readfile("Structure/css/base.css");
-  @readfile("Structure/css/". $theme . ".css");
-  @readfile("Structure/css/tv_2.css");
-  ?>
-  </style>
- </head>
-
- <body id="body" lang="fr">
-  <div class="flex-page">
-   <div class="menu"><?php @include 'Structure/php/modules/'.$lien_menu_theme.'.php'; ?></div>
-   <div class="menu"><?php @include "Structure/php/modules/menu_tv_theme_02.php"; ?></div>
-   <?php @include 'Structure/php/modules/messages_top.php'; ?>
-   <div class="mid">
-
+<?php @readfile("Structure/css/fonts.css");
+@readfile("Structure/css/base.css");
+@readfile("Structure/css/". $theme . ".css");
+@readfile("Structure/css/tv_2.css");
+ ?>
+</style>
+</head>
+<body id="body" lang="fr">
+<div class="flex-page">
+<div class="menu"><?php @include 'Structure/php/modules/'.$lien_menu_theme.'.php'; ?></div>
+<div class="menu"><?php @include "Structure/php/modules/menu_tv_theme_02.php"; ?></div>
+<?php @include 'Structure/php/modules/messages_top.php'; ?>
+<div class="mid">
 <?php
 @include "Structure/php/parsers/lit_tv_2.php";
-$programmes = tv('Structure/cache/tv/xmltv_tnt.xml', date("Hi"), '2359', '1', '0', '360', '10');
+$programmes = tv('Structure/cache/tv/xmltv_tnt.xml', date("Hi", strtotime("-5 minutes")), '2359', '5', '0', '360', '10');
 afficherProgrammeTV($programmes);
 ?>
-
-   </div>
-
-  <div class="messages retour_ligne_on hauteur_auto"><?php @include 'Structure/php/modules/messages.php';?></div>
-  <div class="menu bot"><?php @include "Structure/php/modules/menu_tv_theme_02.php"; ?></div>
-  <div class="menu bot"><?php @include 'Structure/php/modules/'.$lien_menu_theme.'.php'; ?></div>
- </body>
- </html>
+</div>
+<div class="messages retour_ligne_on hauteur_auto"><?php @include 'Structure/php/modules/messages.php';?></div>
+<div class="menu bot"><?php @include "Structure/php/modules/menu_tv_theme_02.php"; ?></div>
+<div class="menu bot"><?php @include 'Structure/php/modules/'.$lien_menu_theme.'.php'; ?></div>
+</body>
+</html>
 <?php $p=ob_get_clean();
 if (substr_count($p,'<!DOCTYPE html>')===1 && substr_count($p,'</html>')===1 && strlen($p)>=1024) {
-    echo $p;
-    @file_put_contents($nom_page.'.html',$p);
-    @chmod($nom_page.'.html',0775);
-    @copy($nom_page.'.html',$nom_page.date("j").'.html');
-    @chmod($nom_page.date("j").'.html',0775);
-} else {
-    @readfile($nom_page.'.html');
-}} else {
-    @readfile($nom_page.'.html');
-}
-?>
-<?php @include 'Structure/php/modules/stats.php';
+echo $p;
+@file_put_contents($nom_page.'.html',$p);
+@chmod($nom_page.'.html',0775);
+@copy($nom_page.'.html',$nom_page.date("j").'.html');
+@chmod($nom_page.date("j").'.html',0775);
+} else {@readfile($nom_page.'.html');}} else {@readfile($nom_page.'.html');}
+?><?php @include 'Structure/php/modules/stats.php';
