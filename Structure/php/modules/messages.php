c2041e9d8b3d16e13cd2010681edc72287bf3a64
Jean-Michel GURGUI <jemi33@gmail.com>
2025-03-07 07:39:19 +0100

    Add files via upload

--- Structure/php/modules/messages.php
+++ Structure/php/modules/messages.php
@@ -1,13 +1,9 @@
 
 <span class="pad8px retour_ligne_on hauteur_auto largeur_auto marges_auto">
- 
-<span> 📎&nbsp;Dernier jour&nbsp;:&nbsp;manque&nbsp;60€ </span>
 
-<span> 👤&nbsp;Donateur : Philippe B. 5€ le 28/02 </span>
+<span><a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊">💳&nbsp;Faire un don </a></span>
 
-<span><a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title="Faire un don, merci par avance 😊"> 💳&nbsp;Faire un don </a></span>
-
-<?php if (!$affiche_message_top) {; ?><a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" alt="Github 😊" title="Github 😊"> 😸&nbsp;Github </a><span id="coucou" class="gris_clair coucou" data-user="33imej" data-website="moc.liamg" title="Coucou 😊" aria-label="Coucou 😊" role="term"></span>
+<?php if (!$affiche_message_top) {; ?><a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" alt="Github 😊" title="Github 😊">🚀&nbsp;Github </a><span id="coucou" class="coucou" data-user="33imej" data-website="moc.liamg" title="Coucou 😊" aria-label="Coucou 😊" role="term"></span>
 
 <?php }; ?>
 </span>
