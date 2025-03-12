8503220e1c1c7f59885e30d87220c33778d03b47
Jean-Michel GURGUI <jemi33@gmail.com>
2025-03-12 21:29:58 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-<?php include 'tec.php'; ?>
+<?php
+$aa = 'tec';
+$ab = 3600;if (!file_exists($aa . '.html') or filemtime($aa . '.html') < (time() - $ab) or !file_exists($aa . date("j") . '.html')) {@include $aa . '.php';} else {@readfile($aa . '.html');@include 'Structure/php/modules/stats.php';}
