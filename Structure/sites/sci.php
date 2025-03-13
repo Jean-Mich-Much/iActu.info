47e4a3fb374abaffa4c8002b75324935a417fdfe
Jean-Michel GURGUI <jemi33@gmail.com>
2025-03-13 21:57:33 +0100

    Add files via upload

--- Structure/sites/sci.php
+++ Structure/sites/sci.php
@@ -1,11 +1,11 @@
-<?php parse('futur', '🚀', 'https://www.futura-sciences.com/', 151, 144, 'Futura',29,'https://iactu.info/recherche/p/i/?a=rss&get=f_36&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('futur', '🚀', 'https://www.futura-sciences.com/', 'Futura',29,'https://iactu.info/recherche/p/i/?a=rss&get=f_36&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('tecsci', '🚀', 'https://www.techno-science.net/', 144, 144, 'Techno-Science',34,'https://iactu.info/recherche/p/i/?a=rss&get=f_39&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('tecsci', '🚀', 'https://www.techno-science.net/', 'Techno-Science',34,'https://iactu.info/recherche/p/i/?a=rss&get=f_39&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('sciav', '🚀', 'https://www.sciencesetavenir.fr/', 144, 144, 'Sciences et Avenir',39,'https://iactu.info/recherche/p/i/?a=rss&get=f_38&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('sciav', '🚀', 'https://www.sciencesetavenir.fr/', 'Sciences et Avenir',39,'https://iactu.info/recherche/p/i/?a=rss&get=f_38&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('scipo', '🚀', 'https://sciencepost.fr/', 144, 144, 'SciencePost',44,'https://iactu.info/recherche/p/i/?a=rss&get=f_37&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('scipo', '🚀', 'https://sciencepost.fr/', 'SciencePost',44,'https://iactu.info/recherche/p/i/?a=rss&get=f_37&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('get8', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_8', 227, 144, 'Autres sites',49,'https://iactu.info/recherche/p/i/?a=rss&get=c_8&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('get8', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_8', 'Autres sites',49,'https://iactu.info/recherche/p/i/?a=rss&get=c_8&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('get9', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_9', 221, 144, 'English',54,'https://iactu.info/recherche/p/i/?a=rss&get=c_9&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('get9', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_9', 'English',54,'https://iactu.info/recherche/p/i/?a=rss&get=c_9&user=Gon1Kirua&token=flux&hours=2400'); ?>
