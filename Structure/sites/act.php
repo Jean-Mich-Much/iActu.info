be9ad3f1f7b95e45fc9da64079ccd323a175326e
Jean-Michel GURGUI <jemi33@gmail.com>
2025-03-13 21:58:18 +0100

    Add files via upload

--- Structure/sites/act.php
+++ Structure/sites/act.php
@@ -1,11 +1,11 @@
-<?php parse('actu4', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_4', 227, 144, 'France',27,'https://iactu.info/recherche/p/i/?a=rss&get=c_4&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('actu4', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_4', 'France',27,'https://iactu.info/recherche/p/i/?a=rss&get=c_4&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('actu6', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_6', 114, 144, 'Monde',32,'https://iactu.info/recherche/p/i/?a=rss&get=f_134&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('actu6', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_6', 'Monde',32,'https://iactu.info/recherche/p/i/?a=rss&get=f_134&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('actu5', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_5', 194, 144, 'Insolites',37,'https://iactu.info/recherche/p/i/?a=rss&get=c_5&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('actu5', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_5', 'Insolites',37,'https://iactu.info/recherche/p/i/?a=rss&get=c_5&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('actu2', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_2', 154, 144, 'TV - Cinéma',42,'https://iactu.info/recherche/p/i/?a=rss&get=c_2&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('actu2', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_2', 'TV - Cinéma',42,'https://iactu.info/recherche/p/i/?a=rss&get=c_2&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('actu10', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_10', 178, 144, 'Sports',47,'https://iactu.info/recherche/p/i/?a=rss&get=c_10&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('actu10', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_10', 'Sports',47,'https://iactu.info/recherche/p/i/?a=rss&get=c_10&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('actu3', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_3', 221, 144, 'English',52,'https://iactu.info/recherche/p/i/?a=rss&get=c_3&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('actu3', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_3', 'English',52,'https://iactu.info/recherche/p/i/?a=rss&get=c_3&user=Gon1Kirua&token=flux&hours=2400'); ?>
--- Structure/sites/tec.php
+++ Structure/sites/tec.php
@@ -1,47 +1,47 @@
-<?php parse('clubi', '🚀', 'https://www.clubic.com/', 144, 144, 'Clubic',20,'https://iactu.info/recherche/p/i/?a=rss&get=f_86&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('clubi', '🚀', 'https://www.clubic.com/', 'Clubic',20,'https://iactu.info/recherche/p/i/?a=rss&get=f_86&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('nextim', '🚀', 'https://next.ink/', 72, 144, 'Next',25,'https://iactu.info/recherche/p/i/?a=rss&get=f_98&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('nextim', '🚀', 'https://next.ink/', 'Next',25,'https://iactu.info/recherche/p/i/?a=rss&get=f_98&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('gennt', '🚀', 'http://www.generation-nt.com/', 144, 144, 'GNT',30,'https://iactu.info/recherche/p/i/?a=rss&get=f_91&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('gennt', '🚀', 'http://www.generation-nt.com/', 'GNT',30,'https://iactu.info/recherche/p/i/?a=rss&get=f_91&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('cowco', '🚀', 'http://www.cowcotland.com/', 178, 144, 'Cowcotland',35,'https://iactu.info/recherche/p/i/?a=rss&get=f_87&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('cowco', '🚀', 'http://www.cowcotland.com/', 'Cowcotland',35,'https://iactu.info/recherche/p/i/?a=rss&get=f_87&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('tomsh', '🚀', 'https://www.tomshardware.fr/', 95, 144, 'Tom&#39;s Hardware',40,'https://iactu.info/recherche/p/i/?a=rss&get=f_105&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('tomsh', '🚀', 'https://www.tomshardware.fr/', 'Tom&#39;s Hardware',40,'https://iactu.info/recherche/p/i/?a=rss&get=f_105&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('tomsg', '🚀', 'https://www.tomsguide.fr/', 95, 144, 'Tom&#39;s Guide',45,'https://iactu.info/recherche/p/i/?a=rss&get=f_104&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('tomsg', '🚀', 'https://www.tomsguide.fr/', 'Tom&#39;s Guide',45,'https://iactu.info/recherche/p/i/?a=rss&get=f_104&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('ginjf', '🚀', 'https://www.ginjfo.com/', 144, 144, 'GinjFo',50,'https://iactu.info/recherche/p/i/?a=rss&get=f_92&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('ginjf', '🚀', 'https://www.ginjfo.com/', 'GinjFo',50,'https://iactu.info/recherche/p/i/?a=rss&get=f_92&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('compt', '🚀', 'https://www.comptoir-hardware.com/', 150, 144, 'Le Comptoir',55,'https://iactu.info/recherche/p/i/?a=rss&get=f_95&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('compt', '🚀', 'https://www.comptoir-hardware.com/', 'Le Comptoir',55,'https://iactu.info/recherche/p/i/?a=rss&get=f_95&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('overc', '🚀', 'https://overclocking.com/', 95, 144, 'Overclocking',60,'https://iactu.info/recherche/p/i/?a=rss&get=f_101&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('overc', '🚀', 'https://overclocking.com/', 'Overclocking',60,'https://iactu.info/recherche/p/i/?a=rss&get=f_101&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('kultu', '🚀', 'https://kulturegeek.fr/', 103, 144, 'KultureGeek',65, 'https://iactu.info/recherche/p/i/?a=rss&get=f_110&user=Gon1Kirua&token=flux&hours=2400');?>
+<?php parse('kultu', '🚀', 'https://kulturegeek.fr/', 'KultureGeek',65, 'https://iactu.info/recherche/p/i/?a=rss&get=f_110&user=Gon1Kirua&token=flux&hours=2400');?>
 
-<?php parse('numer', '🚀', 'https://www.numerama.com/', 150, 144, 'Numerama',70,'https://iactu.info/recherche/p/i/?a=rss&get=f_100&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('numer', '🚀', 'https://www.numerama.com/', 'Numerama',70,'https://iactu.info/recherche/p/i/?a=rss&get=f_100&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('fredz', '🚀', 'http://www.fredzone.org/', 144, 144, 'FZN',75,'https://iactu.info/recherche/p/i/?a=rss&get=f_90&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('omgpu', '🚀', 'https://www.omgpu.com/', 'OmGpu',75,'https://iactu.info/recherche/p/i/?a=rss&get=f_112&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('jourdg', '🚀', 'https://www.journaldugeek.com/', 126, 144, 'Journal du Geek',80,'https://iactu.info/recherche/p/i/?a=rss&get=f_94&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('jourdg', '🚀', 'https://www.journaldugeek.com/', 'Journal du Geek',80,'https://iactu.info/recherche/p/i/?a=rss&get=f_94&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('lesnum', '🚀', 'https://www.lesnumeriques.com/', 150, 144, 'Les Numériques',85,'https://iactu.info/recherche/p/i/?a=rss&get=f_96&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('lesnum', '🚀', 'https://www.lesnumeriques.com/', 'Les Numériques',85,'https://iactu.info/recherche/p/i/?a=rss&get=f_96&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('presc', '🚀', 'https://www.presse-citron.net/', 135, 144, 'Presse-citron',90,'https://iactu.info/recherche/p/i/?a=rss&get=f_102&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('presc', '🚀', 'https://www.presse-citron.net/', 'Presse-citron',90,'https://iactu.info/recherche/p/i/?a=rss&get=f_102&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('infor', '🚀', 'https://www.informaticien.be/', 144, 144, 'Informaticien',95,'https://iactu.info/recherche/p/i/?a=rss&get=f_93&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('infor', '🚀', 'https://www.informaticien.be/', 'Informaticien',95,'https://iactu.info/recherche/p/i/?a=rss&get=f_93&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('devel', '🚀', 'https://www.developpez.com/', 127, 144, 'Developpez',100,'https://iactu.info/recherche/p/i/?a=rss&get=f_88&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('devel', '🚀', 'https://www.developpez.com/', 'Developpez',100,'https://iactu.info/recherche/p/i/?a=rss&get=f_88&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('siedi', '🚀', 'https://siecledigital.fr/', 144, 144, 'Siècle Digital',105,'https://iactu.info/recherche/p/i/?a=rss&get=f_103&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('siedi', '🚀', 'https://siecledigital.fr/', 'Siècle Digital',105,'https://iactu.info/recherche/p/i/?a=rss&get=f_103&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('nexpi', '🚀', 'https://www.nextpit.fr/', 144, 144, 'NextPit',110,'https://iactu.info/recherche/p/i/?a=rss&get=f_99&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('nexpi', '🚀', 'https://www.nextpit.fr/', 'NextPit',110,'https://iactu.info/recherche/p/i/?a=rss&get=f_99&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('frand', '🚀', 'https://www.frandroid.com/', 145, 144, 'FrAndroid',115,'https://iactu.info/recherche/p/i/?a=rss&get=f_89&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('frand', '🚀', 'https://www.frandroid.com/', 'FrAndroid',115,'https://iactu.info/recherche/p/i/?a=rss&get=f_89&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('get18', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_18', 144, 144, 'Réalité virtuelle',120,'https://iactu.info/recherche/p/i/?a=rss&get=c_18&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('get18', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_18', 'Réalité virtuelle',120,'https://iactu.info/recherche/p/i/?a=rss&get=c_18&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('get14', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_14', 144, 144, 'Linux',125,'https://iactu.info/recherche/p/i/?a=rss&get=c_14&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('get14', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_14', 'Linux',125,'https://iactu.info/recherche/p/i/?a=rss&get=c_14&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('get20', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_20', 227, 144, 'Autres sites',130,'https://iactu.info/recherche/p/i/?a=rss&get=c_20&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('get20', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_20', 'Autres sites',130,'https://iactu.info/recherche/p/i/?a=rss&get=c_20&user=Gon1Kirua&token=flux&hours=2400'); ?>
 
-<?php parse('get21', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_21', 221, 144, 'English',135,'https://iactu.info/recherche/p/i/?a=rss&get=c_21&user=Gon1Kirua&token=flux&hours=2400'); ?>
+<?php parse('get21', '🚀', 'https://iactu.info/recherche/p/i/?a=normal&get=c_21', 'English',135,'https://iactu.info/recherche/p/i/?a=rss&get=c_21&user=Gon1Kirua&token=flux&hours=2400'); ?>
