e65a82f694f014fdee753e6ff79d549c072b1316
Jean-Michel GURGUI <jemi33@gmail.com>
2025-03-14 09:21:44 +0100

    Add files via upload

--- theme_02_index.php
+++ theme_02_index.php
@@ -1,1 +1,3 @@
-<?php include 'theme_02_tec.php'; ?>
+<?php
+$aa = 'theme_02_tec';
+$ab = 3600;if (!file_exists($aa . '.html') or filemtime($aa . '.html') < (time() - $ab) or !file_exists($aa . date("j") . '.html')) {@include $aa . '.php';} else {@readfile($aa . '.html');@include 'Structure/php/modules/stats.php';}
