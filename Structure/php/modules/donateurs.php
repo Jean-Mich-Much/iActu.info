c62905dca36ff45c6e0eb9ac1b43c1866f2bca44
Jean-Michel GURGUI <jemi33@gmail.com>
2025-04-02 09:36:57 +0200

    Add files via upload

--- Structure/php/modules/donateurs.php
+++ Structure/php/modules/donateurs.php
@@ -1,6 +1,12 @@
 <!-- 😊 🤗 😄 😅 😀 🙂 😁 😇 😍 🥳 🥰 😜 🤩 😋 😏 😃 😆 -->
+ 
+ <div class="messages retour_ligne_on hauteur_auto">
+ <span class="pad8px retour_ligne_on hauteur_auto largeur_auto marges_auto">
+ <span style="line-height: round(up, 1.35rem, 6px);">🌟 Derniers dons : 🥳 Marc A. 10€, 😅 Andry R. 10€, 😀 Olivier P. 10€, 😏 Mickael E. 10€, 🥰 Philippe D. M. 20€, Matthieu L. 10€</span>
+ </span></div>
+
 
 <div class="messages retour_ligne_on hauteur_auto">
 <span class="pad8px retour_ligne_on hauteur_auto largeur_auto marges_auto">
-<span style="line-height: round(up, 1.35rem, 6px);">🌟 Derniers dons : 🤗 Allan V. 5€&nbsp; 🤩 Jose M. 60€&nbsp;&nbsp; &nbsp; 🔔 Abonnés : 🥰 Johann V. 20€&nbsp; 😋 Philippe D. 10€&nbsp; 😊 Xavier M. 5€&nbsp; 😍 Sébastien D. 5€&nbsp; 🥳 Philippe B. 5€&nbsp; 😜 Nicolas B. 5€&nbsp; </span>
+<span style="line-height: round(up, 1.35rem, 6px);">🔔 Abonnés : 🤗 Damien V. 5€, 🥰 Johann V. 20€&nbsp; 😋 Philippe D. 10€&nbsp; 😊 Xavier M. 5€&nbsp; 😍 Sébastien D. 5€&nbsp; 🥳 Philippe B. 5€&nbsp; 😜 Nicolas B. 5€&nbsp; </span>
 </span></div>
--- Structure/php/modules/messages_top.php
+++ Structure/php/modules/messages_top.php
@@ -1,7 +1,9 @@
 <?php $affiche_message_top = true;?>
 
-<?php if ($affiche_message_top) {; ?>
-
-<div class="messages retour_ligne_on hauteur_auto top"><?php @include 'Structure/php/modules/messages.php'; ?></div>
-
-<?php };
+ <?php if ($affiche_message_top) {; ?>
+<!-- 😊 🤗 😄 😅 😀 🙂 😁 😇 😍 🥳 🥰 😜 🤩 😋 😏 😃 😆 -->
+<div class="messages retour_ligne_on hauteur_auto top">
+<span class="pad8px retour_ligne_on hauteur_auto largeur_auto marges_auto">
+<span style="line-height: round(up, 1.35rem, 6px);">
+🪧&nbspCoucou, cela fait du bien de rentrer, merci pour vos dons et vos messages d'encouragements qui font chauds au cœur 😍
+</span></span></div><?php };
