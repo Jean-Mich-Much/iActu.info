306e74e7dd7adac1064cb2737e795c2d611937ce
Jean-Michel GURGUI <jemi33@gmail.com>
2025-09-14 16:41:55 +0200

    Add files via upload

--- effacecaches.php
+++ effacecaches.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);function effacecache(string $d,int $j):void{$i=$j*86400;$t=time();try{if(!is_dir($d)){throw new InvalidArgumentException("Dossier inexistant : $d");}foreach(new DirectoryIterator($d)as$f){if($f->isFile()){if($f->getMTime()<($t-$i)){unlink($f->getPathname());}}}}catch(Throwable $e){}}; ?>
 
 <?php 
-effacecache('Structure/cache/tv/images/cache/',120);
-effacecache('Structure/cache/source/',120);
-effacecache('Structure/cache/html/',120);
+effacecache('Structure/cache/tv/images/cache/',64);
+effacecache('Structure/cache/source/',64);
+effacecache('Structure/cache/html/',64);
