517af4669f8e23d06b9ff6b0105bfc1652a55834
Jean-Michel GURGUI <jemi33@gmail.com>
2026-03-13 09:00:48 +0100

    Add files via upload

--- Structure/php/modules/header.php
+++ Structure/php/modules/header.php
@@ -1,36 +1,28 @@
 <meta charset="utf-8">
 <title><?php echo $titre_page; ?></title>
+
 <link rel="alternate" href="https://iactu.info/recherche/p/i/?a=rss&user=Gon1Kirua&token=flux&hours=168" type="application/rss+xml" title="RSS - iActu">
+
 <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🚀</text></svg>">
-<link rel="base" href="https://iactu.info/">
-<meta name="abstract" content="<?php echo $titre_page; ?> - Revue de presse">
-<meta name="category" content="news">
-<meta name="classification" content="news">
-<meta name="coverage" content="Worldwide">
+
+<base href="https://iactu.info/">
+
 <meta name="description" content="🚀 <?php echo $titre_page; ?> - 🪧 iActu est une revue de presse des sites technologiques et bien plus 😊">
-<meta name="distribution" content="Global">
 <meta name="format-detection" content="telephone=no">
 <meta name="generator" content="iActu">
-<meta name="geo.placename" content="Paris">
-<meta name="geo.position" content="48.856614;2.3522219000000177">
-<meta name="geo.region" content="FR-75">
-<meta name="keywords" content="iactu,news,actualité,presse,information,journal,magazine,tv">
-<meta name="language" content="FR">
-<meta name="owner" content="Jean-Michel GURGUI">
-<meta name="rating" content="General">
-<meta name="revised" content="<?php echo date('l'); ?>, <?php echo date('F'); ?> <?php echo date('j'); ?>th, <?php echo date('Y'); ?>, <?php echo date('g'); ?>:<?php echo date('i'); ?> <?php echo date('a'); ?>" />
-<meta name="revisit-after" content="7 days">
-<meta name="robots" content="index, follow">
-<meta name="subject" content="<?php echo $titre_page; ?> - Revue de presse des sites technologiques">
-<meta name="url" content="iactu.info">
+
+<meta name="robots" content="index">
+<meta name="googlebot" content="index">
+<meta name="bingbot" content="index">
+
 <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, maximum-scale=5, user-scalable=yes">
-<meta name="msapplication-tap-highlight" content="no">
+
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="mobile-web-app-capable" content="yes">
-<meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="msapplication-starturl" content="/">
-<meta name="HandheldFriendly" content="true">
-<meta name="application-name" content="iActu ">
+
+<meta name="application-name" content="iActu">
 <meta name="apple-mobile-web-app-title" content="iActu">
 <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff">
+
 <link rel="manifest" href="/iactu.webmanifest">
