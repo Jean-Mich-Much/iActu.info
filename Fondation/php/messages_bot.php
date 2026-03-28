f9adbeb0bd1a0aa98119f7612a4a4e669b4e26fa
Jean-Michel GURGUI <jemi33@gmail.com>
2026-03-28 14:36:23 +0100

    Add files via upload

--- Fondation/php/messages_bot.php
+++ Fondation/php/messages_bot.php
@@ -4,7 +4,7 @@
 <?php if ($affiche_message_bot_1) { ?>
 <div>
 <span>
-🪧 La suite du montage d’un autre PC full custom impressionnant qu’il présentera à la prochaine Paris Games Week&nbsp;: <a href="https://www.twitch.tv/videos/2580514769?filter=archives&sort=time" target="_blank" aria-label="Lien 😊" alt="Lien 😊" title=" Lien 😊 " style="display: inline;">&nbsp;🔗&nbsp;Lien&nbsp;Twitch&nbsp;</a>
+🪧 La suite du montage d’un autre PC full custom impressionnant qu’il présentera à la prochaine Paris Games Week&nbsp;: <a href="https://www.twitch.tv/videos/2580514769?filter=archives&sort=time" target="_blank" aria-label="Lien 😊" title=" Lien 😊 " style="display: inline;">&nbsp;🔗&nbsp;Lien&nbsp;Twitch&nbsp;</a>
 </span></div><?php };?>
  
 <?php $affiche_message_bot_2 = true; ?>
@@ -28,9 +28,9 @@
 <?php if ($affiche_message_bot_4) { ?>
 <div>
 <span>
-<a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" alt="Github 😊" title=" Github 😊 " rel="noopener" id="a01">🐱&nbsp;Github </a> &nbsp;&nbsp; 
-<a href="https://codeberg.org/iActuinfo/iActu" target="_blank" aria-label="Codeberg 😊" alt="Codeberg 😊" title=" Codeberg 😊 " rel="noopener" id="a02">📜&nbsp;Codeberg </a> &nbsp;&nbsp; 
-<a href="https://iactu.info/Fondation/doc/index.html" target="_blank" aria-label="Doc 😊" alt="Doc 😊" title=" Doc 😊 " rel="noopener" id="a03">📚&nbsp;Doc </a> &nbsp;&nbsp; 
-<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" alt="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-bot">✨&nbsp;Faire un don&nbsp; </a> &nbsp;&nbsp; 
+<a href="https://github.com/Jean-Mich-Much/iActu.info" target="_blank" aria-label="Github 😊" title=" Github 😊 " rel="noopener" id="a01">🐱&nbsp;Github </a> &nbsp;&nbsp; 
+<a href="https://codeberg.org/iActuinfo/iActu" target="_blank" aria-label="Codeberg 😊" title=" Codeberg 😊 " rel="noopener" id="a02">📜&nbsp;Codeberg </a> &nbsp;&nbsp; 
+<a href="https://iactu.info/Fondation/doc/index.html" target="_blank" aria-label="Doc 😊" title=" Doc 😊 " rel="noopener" id="a03">📚&nbsp;Doc </a> &nbsp;&nbsp; 
+<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-bot">✨&nbsp;Faire un don&nbsp; </a> &nbsp;&nbsp; 
 <span id="coucou" class="coucou" data-user="33imej" data-website="moc.liamg" title=" Coucou 😊 " aria-label="Coucou 😊" role="term"></span>
 </span></div><?php };
