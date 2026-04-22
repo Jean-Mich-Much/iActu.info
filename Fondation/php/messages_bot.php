cbcdbcef14e2187f78f5feed071228eb09db3705
Jean-Michel GURGUI <jemi33@gmail.com>
2026-04-22 20:03:48 +0200

    Add files via upload

--- Fondation/php/messages_bot.php
+++ Fondation/php/messages_bot.php
@@ -4,22 +4,22 @@
 <?php if ($affiche_message_bot_1) { ?>
 <div>
 <span>
-🪧 Coucou 
-</span></div><?php };?>
+🪧 Coucou
+</span></div><?php }?>
  
 <?php $affiche_message_bot_2 = false; ?>
 <?php if ($affiche_message_bot_2) { ?>
 <div>
 <span>
-🪧 Dons, budget&nbsp;de&nbsp;mars (hors&nbsp;abonnés), merci&nbsp;à&nbsp;:  Allan&nbsp;V.&nbsp;5€, Olivier&nbsp;P.&nbsp;10€, Xavier&nbsp;D.&nbsp;10€, Stéphane&nbsp;L.&nbsp;5€, Jérémy&nbsp;B.&nbsp;5€ et Philippe&nbsp;D. M.&nbsp;20€.
-</span></div><?php };?>
+🪧 Coucou
+</span></div><?php }?>
  
 <?php $affiche_message_bot_3 = true; ?>
 <?php if ($affiche_message_bot_3) { ?>
 <div>
 <span>
 🔔&nbsp;Abonnés&nbsp;: Arnaud&nbsp;C.&nbsp;5€, Alain&nbsp;S.&nbsp;5€,  Johann&nbsp;V.&nbsp;20€, Philippe&nbsp;B.&nbsp;5€, Philippe&nbsp;D.&nbsp;10€, Nicolas&nbsp;B.&nbsp;5€, Xavier&nbsp;M.&nbsp;5€, Sébastien&nbsp;D.&nbsp;5€&nbsp;et&nbsp;Damien&nbsp;V.&nbsp;5€
-</span></div><?php };?>
+</span></div><?php }?>
  
 <?php $affiche_message_bot_4 = true; ?>
 <?php if ($affiche_message_bot_4) { ?>
@@ -29,6 +29,6 @@
 <a href="https://codeberg.org/iActuinfo/iActu" target="_blank" aria-label="Codeberg 😊" title=" Codeberg 😊 " rel="noopener" id="a02">📜&nbsp;Codeberg </a> &nbsp;&nbsp; 
 <a href="https://iactu.info/Fondation/doc/index.html" target="_blank" aria-label="Doc 😊" title=" Doc 😊 " rel="noopener" id="a03">📚&nbsp;Doc </a> &nbsp;&nbsp; 
 <a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-bot">✨&nbsp;Faire un don&nbsp; </a> &nbsp;&nbsp; 
-<a href="https://iactu.info/Page_stats.php" aria-label="Statistiques des pages 😊" title=" Statistiques des pages 😊 " rel="noopener" id="stats">🗃️&nbsp;Stats </a> &nbsp;&nbsp; 
+<a href="https://iactu.info/Page_stats.php" aria-label="Statistiques des pages 😊" title=" Statistiques des pages 😊 " rel="noopener" id="stats">&nbsp;🗃️&nbsp;Stats </a> &nbsp;&nbsp; 
 <span id="coucou" class="coucou" data-user="33imej" data-website="moc.liamg" title=" Coucou 😊 " aria-label="Coucou 😊" role="term"></span>
-</span></div><?php };
+</span></div><?php }
--- Fondation/php/messages_top.php
+++ Fondation/php/messages_top.php
@@ -4,19 +4,19 @@
 <?php if ($affiche_message_top_1) { ?>
 <div>
 <span>
-🪧 Bienvenue !&nbsp;<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-top">&nbsp;✨&nbsp;Faire un don&nbsp; </a> &nbsp;<a href="https://iactu.info/Fondation/doc/index.html" target="_blank" rel="noopener noreferrer" id="dochtml" aria-label="Documentation 😊" title=" Documentation 😊 ">&nbsp;📚&nbsp;Doc&nbsp;</a>
-</span></div><?php };?>
+🪧 Bienvenue !&nbsp;<a href="https://www.paypal.com/donate/?hosted_button_id=QVKRNFGMPXYXY" target="_blank" aria-label="Faire un don, merci par avance 😊" title=" Faire un don, merci par avance 😊 " id="paypal-message-top">&nbsp;✨&nbsp;Faire un don&nbsp; </a> &nbsp;<a href="https://iactu.info/Page_stats.php" aria-label="Statistiques des pages 😊" title=" Statistiques des pages 😊 " rel="noopener" id="stats-top">&nbsp;🗃️&nbsp;Stats </a> &nbsp;<a href="https://iactu.info/Fondation/doc/index.html" target="_blank" rel="noopener noreferrer" id="dochtml-top" aria-label="Documentation 😊" title=" Documentation 😊 ">&nbsp;📚&nbsp;Doc&nbsp;</a>
+</span></div><?php }?>
  
 <?php $affiche_message_top_2 = false; ?>
 <?php if ($affiche_message_top_2) { ?>
 <div>
 <span>
-🪧 Coucou 
-</span></div><?php };?>
+🪧 Coucou
+</span></div><?php }?>
  
 <?php $affiche_message_top_3 = false; ?>
 <?php if ($affiche_message_top_3) { ?>
 <div>
 <span>
-🪧 Coucou 
-</span></div><?php };
+🪧 Coucou
+</span></div><?php }
